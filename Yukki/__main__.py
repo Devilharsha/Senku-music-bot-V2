4149c85f6b9ea3687e57dadeb77fbd1af57747e1
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-02-16 19:15:29 +0530

    Update __main__.py

--- Yukki/__main__.py
+++ Yukki/__main__.py
@@ -93,7 +93,7 @@ async def initiate_bot():
             status="[bold blue]Importation Completed!",
         )
     console.print(
-        "[bold green]Congrats!! Yukki Music Bot has started successfully!\n"
+        "[bold green]Congrats!! SENKU Music Bot has started successfully!\n"
     )
     try:
         await app.send_message(
@@ -126,8 +126,8 @@ async def initiate_bot():
             console.print(f"\n[red]Stopping Bot")
             return
         try:
-            await ASS_CLI_1.join_chat("OfficialYukki")
-            await ASS_CLI_1.join_chat("YukkiSupport")
+            await ASS_CLI_1.join_chat("SENKUBOTS")
+            await ASS_CLI_1.join_chat("SENKUCHAT")
         except:
             pass
         console.print(f"├[red] Assistant 1 Started as {ASSNAME1}!")
@@ -145,8 +145,8 @@ async def initiate_bot():
             console.print(f"\n[red]Stopping Bot")
             return
         try:
-            await ASS_CLI_2.join_chat("OfficialYukki")
-            await ASS_CLI_2.join_chat("YukkiSupport")
+            await ASS_CLI_2.join_chat("SENKUBOTS")
+            await ASS_CLI_2.join_chat("SENKUCHAT")
         except:
             pass
         console.print(f"├[red] Assistant 2 Started as {ASSNAME2}!")
@@ -184,7 +184,7 @@ async def initiate_bot():
             return
         try:
             await ASS_CLI_4.join_chat("OfficialYukki")
-            await ASS_CLI_4.join_chat("YukkiSupport")
+            await ASS_CLI_4.join_chat("YukkiSupport") 
         except:
             pass
         console.print(f"├[red] Assistant 4 Started as {ASSNAME4}!")
